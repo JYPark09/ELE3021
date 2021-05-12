6a355f89ba7936101c0a0dbd67b71900cc741069
JYPark09 <jyp10987@gmail.com>
2021-05-12 21:39:18 +0900

    fix: fix files about thread

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -121,11 +121,9 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             set_cpu_share(struct proc*, int);
-
-// thread.c
-int thread_create(thread_t *thread, void *(*start_routine)(void*), void* arg);
-void thread_exit(void *retval);
-int thread_join(thread_t thread, void **retval);
+int             thread_create(thread_t *thread, void *(*start_routine)(void*), void* arg);
+void            thread_exit(void *retval);
+int             thread_join(thread_t thread, void **retval);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
